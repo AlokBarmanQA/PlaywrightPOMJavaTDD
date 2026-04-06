5bc4e60643ecc4b7f65d5f37ffce86c6b732053b
Alok Barman <alokb@mac.mynetworksettings.com>
2026-04-06 01:02:18 -0400

    updated

--- src/main/java/com/qa/freelance/base/PlaywrightConnection.java
+++ src/main/java/com/qa/freelance/base/PlaywrightConnection.java
@@ -27,19 +27,19 @@ public class PlaywrightConnection {
 		
 		switch (browserName.toLowerCase()) {
 		case "chromium":
-			browser = playwright.chromium().launch(new LaunchOptions().setHeadless(false).setSlowMo(1000));
+			browser = playwright.chromium().launch(new LaunchOptions().setHeadless(true));
 			break;
 		case "firefox":
-			browser = playwright.firefox().launch(new LaunchOptions().setHeadless(false).setSlowMo(100));
+			browser = playwright.firefox().launch(new LaunchOptions().setHeadless(true).setSlowMo(100));
 			break;
 		case "safari":
-			browser = playwright.webkit().launch(new LaunchOptions().setHeadless(false).setSlowMo(100));
+			browser = playwright.webkit().launch(new LaunchOptions().setHeadless(true).setSlowMo(100));
 			break;	
 		case "chrome":
-			browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setChannel("chrome").setHeadless(false));
+			browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setChannel("chrome").setHeadless(true));
 			break;	
 		case "edge":
-			browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setChannel("msedge").setHeadless(false));
+			browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setChannel("msedge").setHeadless(true));
 			break;	
 		default:
 			System.out.println("Browser name is not valid ...");
